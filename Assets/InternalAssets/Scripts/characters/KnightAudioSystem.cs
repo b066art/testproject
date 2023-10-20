5d6673f4109ff3b84f65b620c891e0230f06f04f
b066art <b066art@gmail.com>
2023-10-20 15:43:34 +0700

    feat: add localization manager

--- Assets/InternalAssets/Scripts/characters/KnightAudioSystem.cs
+++ Assets/InternalAssets/Scripts/characters/KnightAudioSystem.cs
@@ -2,11 +2,26 @@ using UnityEngine;
 
 public class KnightAudioSystem : MonoBehaviour
 {
+    [SerializeField] private AudioClip helloEn, helloRu;
+
     private AudioSource asKnight;
 
     void Start()
     {
         asKnight = GetComponent<AudioSource>();
+        SetLanguage();
+    }
+
+    void SetLanguage()
+    {
+        if (Application.systemLanguage.ToString() == "Russian")
+        {
+            asKnight.clip = helloRu;
+        }
+        else
+        {
+            asKnight.clip = helloEn;
+        }
     }
 
     void OnMouseDown()
